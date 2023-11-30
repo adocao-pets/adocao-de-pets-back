269984d2c89fc97e5043587b73f20f9a2e2b02aa
Alexandre Coelho <alexandre.010c@gmail.com>
2023-11-30 01:51:17 -0300

    fix partial update

--- src/pet/dto/pet.dto.ts
+++ src/pet/dto/pet.dto.ts
@@ -23,29 +23,32 @@ export class CreatePetDto {
     readonly image?: string;
 
     @ApiProperty()
+    @IsOptional()
     @IsString({ message: 'name is required' })
     readonly name: string;
 
     @ApiProperty()
+    @IsOptional()
     @IsPositive({ message: 'age must be a positive number' })
     readonly age: number;
 
     @ApiProperty({ enum: ['CACHORRO', 'GATO']})
+    @IsOptional()
     @IsString({ message: 'type is required, only CACHORRO or GATO' })
     @IsEnum(PetType)
     readonly type: PetType; //CAT | DOG
 
     @ApiProperty()
     @IsOptional()
+    @IsOptional()
     readonly race: string;
 
     @ApiProperty()
+    @IsOptional()
     @IsString({ message: 'description is required' })
     readonly description: string;
-
-    // readonly isActive: boolean = true;
-    // readonly createdAt: Date = new Date();
-    // readonly updatedAt: Date = new Date();
+    
     @IsNumber()
+    @IsOptional()
     readonly userId: number;
 }
